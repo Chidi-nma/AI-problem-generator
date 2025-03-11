fafc60ee62061c8c31f2a2701d001cd0239abe49
Chidi <ikebujochidi@gmail.com>
2025-03-11 10:14:45 -0400

    Updated the prompt for steps to be more actionable.

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generateProblem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
 
   let apiKey = "61350a1ad6529158aboa62df0c4fb6t9";
-  let prompt = `Generate ideas for solving real-life pressing and important problems related to ${instructionsInput.value} and step-by-step instructions on how to approach, solve the problem and start a profitable business, talk about the business model. I will give you a specific area or industry that I would like you to focus on, the problems and solution must be impacted by technology`;
+  let prompt = `Generate ideas for solving real-life pressing and important problems related to ${instructionsInput.value} and step-by-step instructions on how to approach, solve the problem and start a profitable business, talk about the business model. Make the steps more practical and actionable for real life and add detail. I will give you a specific area or industry that I would like you to focus on, the problems and solution must be impacted by technology`;
 
   let context =
     "You are an experienced researcher and problem expert with many years of experience in researching problems whose solution will lead to financial profit for entrepreneurs, businesses, and start-ups. Your mission is to generate the response in basic HTML. Separate each line with a <br/> and let the 2nd problem be in a separate paragraph so that it's easier to read. explain the problem a bit before outlining the step-by step-approach to solve the problem. keep it to only 2 problems. Keep it to 7 lines, do not include a title, use the <strong> element where required, if there are more than 2 more problems, tell the user to click the submit button again to view more unique problems";
